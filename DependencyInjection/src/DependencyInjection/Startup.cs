6177c8809f1dfe45f7280e6bc4a3f9a4edd1c1d8
mahesh <mahesh0919@gmail.com>
2016-09-04 17:08:28 +0530

    Setting up the MVC
    In this step
    1) Added MVC reference in project.json
    2) Added Controllers, Models and Views
    3) Added MVC dependencies in Startup.cs

--- DependencyInjection/src/DependencyInjection/Startup.cs
+++ DependencyInjection/src/DependencyInjection/Startup.cs
@@ -13,10 +13,22 @@ namespace DependencyInjection
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         public void Configure(IApplicationBuilder app)
         {
+            // Add MVC to the request pipeline.
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+
+                // Uncomment the following line to add a route for porting Web API 2 controllers.
+                // routes.MapWebApiRoute("DefaultApi", "api/{controller}/{id?}");
+            });
+
             app.Run(async (context) =>
             {
                 await context.Response.WriteAsync("Hello World!");
