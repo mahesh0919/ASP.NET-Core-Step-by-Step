a1ff7e501d51671b420e3fbdac4c3e965f362c4e
mahesh <mahesh0919@gmail.com>
2016-09-07 10:15:46 +0530

    Adding ViewComponent
    New to ASP.NET Core MVC, view components are similar to partial views,
    but they are much more powerful. View components don’t use model
    binding, and only depend on the data you provide when calling into it. A
    view component:
    
    Renders a chunk rather than a whole response
    Includes the same separation-of-concerns and testability benefits found
    between a controller and view
    Can have parameters and business logic
    Is typically invoked from a layout page

---
+++ DependencyInjection/src/DependencyInjection/ViewComponent/ProductDetailsViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNet.Mvc;
+using DependencyInjection.Services;
+using System.Collections.Generic;
+using DependencyInjection.Models;
+using System.Linq;
+
+namespace WebApplication1.Controllers
+{
+    public class ProductDetailsViewComponent : ViewComponent
+    {
+        private readonly IDBFactory _prodFactory;
+        public ProductDetailsViewComponent(IDBFactory prodFactory)
+        {
+            _prodFactory = prodFactory;
+        }
+
+        public IViewComponentResult Invoke(int id)
+        {
+            List<Product> productList = _prodFactory.GetAllProducts();
+            Product prod = (from p in productList
+                            select p).FirstOrDefault();
+
+            return View(prod);
+        }
+    }
+}
